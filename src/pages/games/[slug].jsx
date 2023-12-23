6396e51c785355edfd56146c5c901a777fd635e3
tina otarashvili <tinatinotarashvili99@gmail.com>
2023-12-23 22:53:02 +0400

    update

--- src/pages/games/[slug].jsx
+++ src/pages/games/[slug].jsx
@@ -254,9 +254,9 @@ export default function Home({ game }) {
 											{game.videos !== null &&
 											<div className="mx-auto">
 											<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-8 pb-6">
-																								{game.videos.map((video) => {
+													{game.videos.map((video) => {
 													return (
-														<div>
+														<div key={video.title}>
 														<iframe
 															src={video.link}
 															className="w-full  aspect-video rounded-2xl shadow-md mb-2"
